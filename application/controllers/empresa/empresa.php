5fb269c9e9e294eacc3ddd144d67a1172d8adc81
gobernacion <gobernacion@172.16.65.58>
2015-04-23 15:37:04 -0430

    Avances del dia 23-04-2015; Ya trae los combos pero aun no actualiza... debo terminar eso.

--- application/controllers/empresa/empresa.php
+++ application/controllers/empresa/empresa.php
@@ -1,194 +1,216 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+<?php
+
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
+
 class Empresa extends CI_Controller {
-  public function __construct(){
-    parent::__construct();
-    session_start();
-    $this->load->helper('url');
-    $this->load->database();
-    $this->load->model('empresa/empresa_model');
-    $this->load->library(array('session'));
-  }
-  public function index() {
-    //$this->load->view('administrativo');
-  }
-  public function Registro(){
-    $resultdbd= array(); 
-    if ($resultdbd=$this->empresa_model->buscaregistro()){
-      $this->output->set_content_type('application/json');
-      $this->output->set_output(json_encode(array(
-      "success" => true,
-      'data' => $resultdbd)));
-    }else{
-      $this->output->set_content_type('application/json');
-      $this->output->set_output(json_encode(array(
-      "success" => false,
-      'data' => $resultdbd)));
+
+    public function __construct() {
+        parent::__construct();
+        session_start();
+        $this->load->helper('url');
+        $this->load->database();
+        $this->load->model('empresa/empresa_model');
+        $this->load->library(array('session'));
     }
-  }
 
-  public function Riflista(){
-   $resultdbd= array(); 
-    if ($resultdbd=$this->empresa_model->buscarrif()){
-      $this->output->set_content_type('application/json');
-      $this->output->set_output(json_encode(array(
-      "success" => true,
-      'data' => $resultdbd)));
-    }else{
-      $this->output->set_content_type('application/json');
-      $this->output->set_output(json_encode(array(
-      "success" => false,
-      'data' => $resultdbd)));
+    public function index() {
+        //$this->load->view('administrativo');
     }
-  }
-  public function guardarempresa() {
-    $empresa=array();
-    $id=$this->input->post('id');
-    if($this->input->post('seleccioncamara1')=='true'){
-      $seleccioncamara1=1;
-      }else{
-        $seleccioncamara1=0;
-      };
 
-    if($this->input->post('seleccioncamara2')=='true'){
-      $seleccioncamara2=1;
-      }else{
-        $seleccioncamara2=0;
-      };
-
-    if($this->input->post('seleccioncamara3')=='true'){
-      $seleccioncamara3=1;
-      }else{
-        $seleccioncamara3=0;
-      };
-
-    if($this->input->post('seleccioncamara4')=='true'){
-      $seleccioncamara4=1;
-      }else{
-        $seleccioncamara4=0;
-      };
-    $empresa=array(
-      'id'              => $this->input->post('id'),
-      'rif'             => $this->input->post('rif'). $this->input->post('rif1').$this->input->post('rif2'),
-      'nombreco'        => $this->input->post('nombrecomer'),
-      'anoact'          =>$this->input->post('anoact'),
-      'registromer'     =>$this->input->post('registromer'),
-      'razonsoc'        =>$this->input->post('razonsoc'),
-      'nacionalidarep'  =>$this->input->post('nacionalidadrep'),
-      'cedularep'       =>$this->input->post('cedularep'),
-      'nombrerep'       =>$this->input->post('representante'),
-      'telfrep'        =>$this->input->post('codmovilrep').$this->input->post('movilrep'),
-      'tipo'            =>$this->input->post('tipo'),
-      'nombrecont'      =>$this->input->post('nombrecont'),
-      'telfcont'        =>$this->input->post('codmovilcont').$this->input->post('movilcont'),
-      'nacionalidadcont' =>$this->input->post('nacionalidadcont'),
-      'cedulacont'       =>$this->input->post('cedulacont'),
-      'estado'          =>$this->input->post('cmbestado'),
-      'municipio'       =>$this->input->post('cmbmunicipio'),
-      'parroquia'       =>$this->input->post('cmbparroquia'),
-      'comunidad'       =>$this->input->post('cmbcomunidad'),
-      'direccion'       =>$this->input->post('direccion'),
-      'tlfmovil'        =>$this->input->post('codmovilemp').$this->input->post('movilemp'),
-      'tlflocal'        =>$this->input->post('codfijoemp').$this->input->post('fijoemp'),
-      'faxemp'          =>$this->input->post('codfaxemp').$this->input->post('faxemp'),
-      'emailemp'        =>$this->input->post('correoemp'),
-      'pagwebemp'       =>$this->input->post('pagwebemp'),
-      'facebemp'        =>$this->input->post('facebookemp'),
-      'twitter'         =>$this->input->post('twitteremp'),
-      'selecamara1'     =>$seleccioncamara1,
-      'selecamara2'     =>$seleccioncamara2,
-      'selecamara3'     =>$seleccioncamara3,
-      'selecamara4'     =>$seleccioncamara4,
-      'seccion'         =>$this->input->post('cmbseccion'),
-      'divisionact'     =>$this->input->post('cmbdivision'),
-      'grupoact'        =>$this->input->post('cmbgrupo'),
-      'claseact'        =>$this->input->post('cmbclase'),
-    );
-    if($id==null){
-      $result2=$this->empresa_model->insertarempresa($empresa);
-      if($result2){
-          echo json_encode(array(
-              "success"   => true,
-              "msg"       => "Se Guardo con Éxito." //modificado en la base de datos
-          ));
-      }else{
-          echo json_encode(array(
-              "success"   => false,
-              "msg"       => "No se puedo Guardar." //no se modifico en la base de datos
-          ));
-      }
-    }else{
-
+    public function Registro() {
+        $resultdbd = array();
+        if ($resultdbd = $this->empresa_model->buscaregistro()) {
+            $this->output->set_content_type('application/json');
+            $this->output->set_output(json_encode(array(
+                "success" => true,
+                'data' => $resultdbd)));
+        } else {
+            $this->output->set_content_type('application/json');
+            $this->output->set_output(json_encode(array(
+                "success" => false,
+                'data' => $resultdbd)));
+        }
     }
-  }
-   public function obtenerEmpresa(){
+
+    public function Riflista() {
+        $resultdbd = array();
+        if ($resultdbd = $this->empresa_model->buscarrif()) {
+            $this->output->set_content_type('application/json');
+            $this->output->set_output(json_encode(array(
+                "success" => true,
+                'data' => $resultdbd)));
+        } else {
+            $this->output->set_content_type('application/json');
+            $this->output->set_output(json_encode(array(
+                "success" => false,
+                'data' => $resultdbd)));
+        }
+    }
+
+    public function guardarempresa() {
+        $empresa = array();
+        $id = $this->input->post('id');
+        if ($this->input->post('seleccioncamara1') == 'true') {
+            $seleccioncamara1 = 1;
+        } else {
+            $seleccioncamara1 = 0;
+        };
+
+        if ($this->input->post('seleccioncamara2') == 'true') {
+            $seleccioncamara2 = 1;
+        } else {
+            $seleccioncamara2 = 0;
+        };
+
+        if ($this->input->post('seleccioncamara3') == 'true') {
+            $seleccioncamara3 = 1;
+        } else {
+            $seleccioncamara3 = 0;
+        };
+
+        if ($this->input->post('seleccioncamara4') == 'true') {
+            $seleccioncamara4 = 1;
+        } else {
+            $seleccioncamara4 = 0;
+        };
+        $empresa = array(
+            'id' => $this->input->post('id'),
+            'rif' => $this->input->post('rif') . $this->input->post('rif1') . $this->input->post('rif2'),
+            'nombreco' => $this->input->post('nombrecomer'),
+            'anoact' => $this->input->post('anoact'),
+            'registromer' => $this->input->post('registromer'),
+            'razonsoc' => $this->input->post('razonsoc'),
+            'nacionalidarep' => $this->input->post('nacionalidadrep'),
+            'cedularep' => $this->input->post('cedularep'),
+            'nombrerep' => $this->input->post('representante'),
+            'telfrep' => $this->input->post('codmovilrep') . $this->input->post('movilrep'),
+            'tipo' => $this->input->post('tipo'),
+            'nombrecont' => $this->input->post('nombrecont'),
+            'telfcont' => $this->input->post('codmovilcont') . $this->input->post('movilcont'),
+            'nacionalidadcont' => $this->input->post('nacionalidadcont'),
+            'cedulacont' => $this->input->post('cedulacont'),
+            'estado' => $this->input->post('cmbestado'),
+            'municipio' => $this->input->post('cmbmunicipio'),
+            'parroquia' => $this->input->post('cmbparroquia'),
+            'comunidad' => $this->input->post('cmbcomunidad'),
+            'direccion' => $this->input->post('direccion'),
+            'tlfmovil' => $this->input->post('codmovilemp') . $this->input->post('movilemp'),
+            'tlflocal' => $this->input->post('codfijoemp') . $this->input->post('fijoemp'),
+            'faxemp' => $this->input->post('codfaxemp') . $this->input->post('faxemp'),
+            'emailemp' => $this->input->post('correoemp'),
+            'pagwebemp' => $this->input->post('pagwebemp'),
+            'facebemp' => $this->input->post('facebookemp'),
+            'twitter' => $this->input->post('twitteremp'),
+            'selecamara1' => $seleccioncamara1,
+            'selecamara2' => $seleccioncamara2,
+            'selecamara3' => $seleccioncamara3,
+            'selecamara4' => $seleccioncamara4,
+            'seccion' => $this->input->post('cmbseccion'),
+            'divisionact' => $this->input->post('cmbdivision'),
+            'grupoact' => $this->input->post('cmbgrupo'),
+            'claseact' => $this->input->post('cmbclase'),
+        );
+
+        if ($id == null) {
+            $result2 = $this->empresa_model->insertarempresa($empresa);
+            if ($result2) {
+                echo json_encode(array(
+                    "success" => true,
+                    "msg" => "Se Guardo con Éxito." //modificado en la base de datos
+                ));
+            } else {
+                echo json_encode(array(
+                    "success" => false,
+                    "msg" => "No se puedo Guardar." //no se modifico en la base de datos
+                ));
+            }
+        } else {
+            $result3 = actualizarempresa($empresa['rif'], $empresa);
+            if ($result3) {
+                echo json_encode(array(
+                    "success" => true,
+                    "msg" => "Se Aptualizo con Éxito." //modificado en la base de datos
+                ));
+            } else {
+                echo json_encode(array(
+                    "success" => false,
+                    "msg" => "No se puedo Aptualizar." //no se modifico en la base de datos
+                ));
+            }
+        }
+    }
+
+    public function obtenerEmpresa() {
         $username = $this->session->userdata('datasession');
-        if($username['login_ok']==TRUE){
+        if ($username['login_ok'] == TRUE) {
             $usuario = $username['usuario'];
             //$nacionalidad =$username['nacionalidad']; 
-        }else{
+        } else {
             //$usuario= $this->input->get("cedula");
             //$nacionalidad= $this->input->get("nacionalidad");
         }
-        $empresa=$this->empresa_model->obtenerEmpresa($usuario);
-        if($empresa!=false){
-            if($username['login_ok']==TRUE){
-                foreach ($empresa->result_array() as $row){
-                  $data[] =array(
-                  'id'              =>$row['id'],
-                  'rif'             =>SUBSTR($row['rif'],0,1),
-                  'rif1'            =>SUBSTR($row['rif'],1,strlen($row['rif'])-2),
-                  'rif2'            =>$row['rif'][strlen($row['rif'])-1],
-                  'nombrecomer'     =>$row['nombreco'],
-                  'anoact'          =>$row['anoact'],
-                  'registromer'     =>$row['registromer'],
-                  'razonsoc'        =>$row['razonsoc'],
-                  'nacionalidadrep'  =>$row['nacionalidarep'],
-                  'cedularep'       =>$row['cedularep'],
-                  'representante'   =>$row['nombrerep'],
-                  'codmovilrep'     => SUBSTR($row['telfrep'],0,3),
-                  'movilrep'        => SUBSTR($row['telfrep'],3),
-                  'tipo'            =>$row['tipo'],
-                  'nombrecont'      =>$row['nombrecont'],
-                  'codmovilcont'    => SUBSTR($row['telfcont'],0,3),
-                  'movilcont'       => SUBSTR($row['telfcont'],3),
-                  'nacionalidadcont'=>$row['nacionalidadcont'],
-                  'cedulacont'      =>$row['cedulacont'],
-                  'cmbestado'       =>$row['estado'],
-                  'cmbmunicipio'    =>$row['municipio'],
-                  'cmbparroquia'    =>$row['parroquia'],
-                  'cmbcomunidad'    =>$row['comunidad'],
-                  'direccion'       =>$row['direccion'],
-                  'codmovilemp'     => SUBSTR($row['tlfmovil'],0,3),
-                  'movilemp'        => SUBSTR($row['tlfmovil'],3),
-                  'codfijoemp'      => SUBSTR($row['tlflocal'],0,3),
-                  'fijoemp'         => SUBSTR($row['tlflocal'],3),
-                  'codfaxemp'       => SUBSTR($row['faxemp'],0,3),
-                  'faxemp'          => SUBSTR($row['faxemp'],3),
-                  'correoemp'       =>$row['emailemp'],
-                  'pagwebemp'       =>$row['pagwebemp'],
-                  'facebookemp'     =>$row['facebemp'],
-                  'twitteremp'      =>$row['twitter'],
-                  'seleccioncamara1' =>$row['selecamara1'],
-                  'seleccioncamara2'     =>$row['selecamara2'],
-                  'seleccioncamara3'     =>$row['selecamara3'],
-                  'seleccioncamara4'     =>$row['selecamara4'],
-                  'cmbseccion'      =>$row['seccion'],
-                  'cmbdivisionact'  =>$row['divisionact'],
-                  'cmbgrupo'        =>$row['grupoact'],
-                  'cmbclase'        =>$row['claseact'],
-                  'total'            =>1
-                  );
+        $empresa = $this->empresa_model->obtenerEmpresa($usuario);
+        if ($empresa != false) {
+            if ($username['login_ok'] == TRUE) {
+                foreach ($empresa->result_array() as $row) {
+                    $data[] = array(
+                        'id' => $row['id'],
+                        'rif' => SUBSTR($row['rif'], 0, 1),
+                        'rif1' => SUBSTR($row['rif'], 1, strlen($row['rif']) - 2),
+                        'rif2' => $row['rif'][strlen($row['rif']) - 1],
+                        'nombrecomer' => $row['nombreco'],
+                        'anoact' => $row['anoact'],
+                        'registromer' => $row['registromer'],
+                        'razonsoc' => $row['razonsoc'],
+                        'nacionalidadrep' => $row['nacionalidarep'],
+                        'cedularep' => $row['cedularep'],
+                        'representante' => $row['nombrerep'],
+                        'codmovilrep' => SUBSTR($row['telfrep'], 0, 3),
+                        'movilrep' => SUBSTR($row['telfrep'], 3),
+                        'tipo' => $row['tipo'],
+                        'nombrecont' => $row['nombrecont'],
+                        'codmovilcont' => SUBSTR($row['telfcont'], 0, 3),
+                        'movilcont' => SUBSTR($row['telfcont'], 3),
+                        'nacionalidadcont' => $row['nacionalidadcont'],
+                        'cedulacont' => $row['cedulacont'],
+                        'cmbestado' => $row['estado'],
+                        'cmbmunicipio' => $row['municipio'],
+                        'cmbparroquia' => $row['parroquia'],
+                        'cmbcomunidad' => $row['comunidad'],
+                        'direccion' => $row['direccion'],
+                        'codmovilemp' => SUBSTR($row['tlfmovil'], 0, 3),
+                        'movilemp' => SUBSTR($row['tlfmovil'], 3),
+                        'codfijoemp' => SUBSTR($row['tlflocal'], 0, 3),
+                        'fijoemp' => SUBSTR($row['tlflocal'], 3),
+                        'codfaxemp' => SUBSTR($row['faxemp'], 0, 3),
+                        'faxemp' => SUBSTR($row['faxemp'], 3),
+                        'correoemp' => $row['emailemp'],
+                        'pagwebemp' => $row['pagwebemp'],
+                        'facebookemp' => $row['facebemp'],
+                        'twitteremp' => $row['twitter'],
+                        'seleccioncamara1' => $row['selecamara1'],
+                        'seleccioncamara2' => $row['selecamara2'],
+                        'seleccioncamara3' => $row['selecamara3'],
+                        'seleccioncamara4' => $row['selecamara4'],
+                        'cmbseccion' => $row['seccion'],
+                        'cmbdivisionact' => $row['divisionact'],
+                        'cmbgrupo' => $row['grupoact'],
+                        'cmbclase' => $row['claseact'],
+                        'total' => 1
+                    );
                 }
             }
-        }else{
+        } else {
             $data[] = array(
-                'total'  =>0
+                'total' => 0
             );
         }
         $output = array(
             'success' => true,
-            'data'    => $data,
-            'total'   => count($data));
+            'data' => $data,
+            'total' => count($data));
         echo json_encode($output);
     }
+
 }
--- application/models/empresa/empresa_model.php
+++ application/models/empresa/empresa_model.php
@@ -1,53 +1,66 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
- class Empresa_model extends CI_Model {
-  function __construct(){
-    $this->load->database();
-    $this->load->library('session');
-     $this->load->library(array('session'));
-  }
-  function buscarrif(){
-    $query = $this->db->query("SELECT usuariolinea.id as id,usuariolinea.cedula as cedula,usuariolinea.correo as correo,usuariolinea.rif as rif,usuariolinea.status as status,usuariolinea.nacionalidad as nacionalidad
+<?php
+
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
+
+class Empresa_model extends CI_Model {
+
+    function __construct() {
+        $this->load->database();
+        $this->load->library('session');
+        $this->load->library(array('session'));
+    }
+
+    function buscarrif() {
+        $query = $this->db->query("SELECT usuariolinea.id as id,usuariolinea.cedula as cedula,usuariolinea.correo as correo,usuariolinea.rif as rif,usuariolinea.status as status,usuariolinea.nacionalidad as nacionalidad
     FROM usuariolinea
     WHERE usuariolinea.status=0");
-    $resultado = array();
-    $resultdb=array();  
-    if ($query->num_rows() > 0){
-      foreach ($query->result() as $row)
-      {
-        $resultado[] = $row;
-      }
-      return $resultado;
-      $query->free-result();
-    } 
-  }
-  public function obtenerEmpresa($usuario){
-        $sql= "SELECT * FROM empresa WHERE empresa.rif=? ";
-        $consulta=$this->db->query($sql,array($usuario));
-        if($consulta->num_rows() == 1){
-            return $consulta;
-        }else{
-            return false;
-        }   
+        $resultado = array();
+        $resultdb = array();
+        if ($query->num_rows() > 0) {
+            foreach ($query->result() as $row) {
+                $resultado[] = $row;
+            }
+            return $resultado;
+            $query->free - result();
+        }
     }
-  function buscaregistro(){
-    $query = $this->db->query("SELECT registro.id as id,registro.nombre as nombre
-    FROM registro");
-    $resultado = array();
-    $resultdb=array();  
-    if ($query->num_rows() > 0){
-      foreach ($query->result() as $row)
-      {
-        $resultado[] = $row;
-      }
-      return $resultado;
-      $query->free-result();
-    }  
-  }  
-  function insertarempresa($empresa){
-    $this->db->set($empresa);
-    $this->db->insert('empresa');
-   //return  mysql_insert_id();
-    return $this->db->insert_id();
-  }
-}
 
+    public function obtenerEmpresa($usuario) {
+        $sql = "SELECT * FROM empresa WHERE empresa.rif=? ";
+        $consulta = $this->db->query($sql, array($usuario));
+        if ($consulta->num_rows() == 1) {
+            return $consulta;
+        } else {
+            return false;
+        }
+    }
+
+    function buscaregistro() {
+        $query = $this->db->query("SELECT registro.id as id,registro.nombre as nombre
+    FROM registro");
+        $resultado = array();
+        $resultdb = array();
+        if ($query->num_rows() > 0) {
+            foreach ($query->result() as $row) {
+                $resultado[] = $row;
+            }
+            return $resultado;
+            $query->free - result();
+        }
+    }
+
+    function insertarempresa($empresa) {
+        $this->db->set($empresa);
+        $this->db->insert('empresa');
+        //return  mysql_insert_id();
+        return $this->db->insert_id();
+    }
+
+    function actualizarempresa($rif, $empresa) {
+        $this->db->set($empresa);
+        $this->db->where('rif', $rif);
+        return $this->db->update('empresa');
+    }
+
+}
